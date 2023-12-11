bcb28a1c9719adff83ff1e9bbebc6b99f5d16424
jatomich <jatomic@spareparse.com>
2023-12-10 18:42:01 -0600

    "STUPID GUI COMMIT MESSAGE"

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,25 @@
 from flask import Flask
-from config import Config, DevelopmentConfig
+from flask_cors import CORS
+from config import Config, DevelopmentConfig, basedir
 from app.models import db
 
+cors = CORS()
 
 def create_app(config_class: Config=DevelopmentConfig):
-    app = Flask(__name__)
-    app.config.from_object(DevelopmentConfig)
+    app = Flask(__name__,
+                instance_path=basedir,
+                instance_relative_config=True,
+                static_url_path='/home/at/Documents/jatRESTapi/app/static',
+                static_folder='/static',
+                template_folder='/app/templates'
+                )
+    app.config.from_object(config_class)
 
+    cors.init_app(app)
     db.init_app(app)
 
     with app.app_context():
-        from app import models, routes
+        from . import models, routes
         db.create_all()
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,20 @@
-from flask import jsonify, current_app as app
+from flask import jsonify, current_app as app, url_for
+import pandas as pd
 
 @app.route('/')
-@app.route('/index')
-def index():
+@app.route('/api')
+def home():
     return jsonify({'message': {'first': 'Hello', 'second': 'World'}})
 
 
 @app.route('/api/v1/movies')
 def apeye():
-    return jsonify({'message': {'title': 'The Shawshank Redemption', 'year': '1994', 'director': 'Frank Darabont'}})
+    return jsonify({'movies': [{'title': 'The Shawshank Redemption', 'year': '1994', 'director': 'Frank Darabont'}]})
+
+
+@app.route('/api/v1/netflix')
+def netflix():
+    df = pd.read_csv(url_for('static', filename='input/netflix_ss.csv'))
+    return_data = {'data': [record for record in df.head().to_dict(orient='records')]}
+    print(return_data)
+    return jsonify(return_data)
