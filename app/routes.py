40654378f859a60a1ee398ab691fb8e13f02af3b
jatomich <jatomic@spareparse.com>
2023-12-11 00:55:10 -0600

    Add route to select content by index.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import jsonify, current_app as app, url_for
+import json
 import pandas as pd
 
 @app.route('/')
@@ -15,6 +16,14 @@ def apeye():
 @app.route('/api/v1/netflix')
 def netflix():
     df = pd.read_csv(url_for('static', filename='input/netflix_ss.csv'))
-    return_data = {'data': [record for record in df.head().to_dict(orient='records')]}
+    df = df.dropna().sort_values(by=['type', 'title']).reset_index(drop=False).copy()
+    return_data = {'data': [record for record in df.to_dict(orient='records')]}
     print(return_data)
     return jsonify(return_data)
+
+
+@app.route('/api/v1/netflix/<int:index>', methods=['GET'])
+def netflix_item(index):
+    df = pd.read_csv(url_for('static', filename='input/netflix_ss.csv'))
+    item = df.iloc[index].to_dict()
+    return jsonify(item)
--- frontend-app/src/components/NetflixItem.js
+++ frontend-app/src/components/NetflixItem.js
@@ -1,21 +1,79 @@
 import React, { Component } from 'react';
 import instance from '../axios';
 
+import Array from 'react-bootstrap/Array';
+
 class NetflixItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      item: {}
+      data: {}
     };
   }}
 
-  componentDidMount() {
+  const fetchData = async () => {
+    const response = await instance.get("/netflix");
+    try {
+      const data = await response.json();
+      this.setState({ data: data });
+    } catch (error) {
+      console.error(error);
+    }
+};
+
+  componentDidMount(); {
     instance.get('/netflix')
       .then(res => {
         // console.log(res.data);
-        this.setState({ item: res.data });
+        this.setState({ data: res.data });
       })
       .catch(err => {
         console.error(err);
       });
+  }; // Add a semicolon at the end of the method declaration
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  render(); {
+    // console.log(this.state.movies); // Move this line outside of the JSX code block
+    const { data } = this.state; // Destructure movies from state
+    return (
+      <>
+        {Array.isArray(data) && data.map((datum, index) => ( // Add check for movies array
+          <li key={index}>
+            <h2>{datum.title}</h2>
+            <p>{datum.director}</p>
+            <p>{datum.description}</p>
+          </li>
+        ))}
+      </>
+    );
   }
+
+export default NetflixItem;
+
+
+      // <div>
+      //   <h1>Movies</h1>
+      //   <table>
+      //     <thead>
+      //       <tr>
+      //         <th>Title</th>
+      //         <th>Director</th> {/* Changed from Director to Genre */}
+      //         <th>Description</th>
+      //       </tr>
+      //     </thead>
+      //     <tbody>
+      //       {Array.isArray(movies) && movies.map((movie, index) => ( // Add check for movies array
+      //         <tr key={index}>
+      //           <td>{movie.title}</td>
+      //           <td>{movie.title}</td>
+      //           <td>{movie.director}</td>
+      //         </tr>
+      //       ))}
+      //     </tbody>
+      //   </table>
+      // </div>
